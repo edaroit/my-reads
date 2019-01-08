20a37140bb7cb85fcb4198791c1d9bb9ac5fd85b
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-08 20:14:47 -0200

    feat: Add Shelves and Search to App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Shelves from './components/Shelves'
+import Search from './containers/Search'
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+class App extends Component {
+  state = {
+    books: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+    },
+  }
+
+  async componentDidMount() {
+    const books = await BooksAPI.getAll()
+    const currentlyReading = books.filter(book => book.shelf === 'currentlyReading')
+    const wantToRead = books.filter(book => book.shelf === 'wantToRead')
+    const read = books.filter(book => book.shelf === 'read')
+    this.setState({ books: { currentlyReading, wantToRead, read } })
+  }
+
+  addBook = async (bookId, shelf) => {
+    const book = await BooksAPI.get(bookId)
+    await BooksAPI.update(book, shelf)
+    this.setState(currentState => ({
+      books: {
+        ...currentState.books,
+        [shelf]: [...currentState.books[shelf], book],
+      },
+    }))
+  }
+
+  render() {
+    const { books } = this.state
+
+    console.log(books)
+
+    return (
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => <Shelves {...books} />}
+        />
+        <Route
+          exact
+          path="/search"
+          render={({ history }) => (
+            <Search
+              onSelect={(bookId, shelf) => {
+                this.addBook(bookId, shelf)
+                history.push('/')
+              }}
+            />
+          )}
+        />
+      </div>
+    )
+  }
+}
 
 export default App
