6434aed9e1eb7f6c8ac3485bcd75d6162d2d9dff
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-20 21:21:38 -0200

    feat: Wrap Shelf with Card

--- src/components/Shelf/index.js
+++ src/components/Shelf/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Book from '../Book'
+import Card from '../Card'
 
 import './shelf.css'
 
 const Shelf = ({ title, books, onSelect }) => (
-  <div className="shelf">
-    <h1 className="shelf__title">
-      {title}
-    </h1>
-    <div className="shelf__books">
-      {
-        books.map(book => (<Book key={book.id} {...book} onSelect={onSelect} />))
-      }
+  <Card>
+    <div className="shelf">
+      <h1 className="shelf__title">
+        {title}
+      </h1>
+      <div className="shelf__books">
+        {
+          books.map(book => (<Book key={book.id} {...book} onSelect={onSelect} />))
+        }
+      </div>
     </div>
-  </div>
+  </Card>
 )
 
 export default Shelf
