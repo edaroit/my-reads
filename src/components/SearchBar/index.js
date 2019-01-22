eb3c4bfba50f4d523b983c89d6f8025d3e72fd39
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-22 08:45:30 -0200

    feat: Add className props to SearchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import cn from 'classnames'
 
 import './search-bar.css'
 
-const SearchBar = ({ onChange }) => (
-  <input className="search-bar" onChange={onChange} placeholder="Search" />
+const SearchBar = ({ className, onChange }) => (
+  <input
+    className={cn('search-bar', className && className)}
+    onChange={onChange}
+    placeholder="Search"
+  />
 )
 
 export default SearchBar
