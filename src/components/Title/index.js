34204601d23bdc10fd6d81097d23c9cbd8422515
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-22 08:52:29 -0200

    feat: Add className props to Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import cn from 'classnames'
 
 import './title.css'
 
-const Title = ({ children, as: Tag = 'h1', align = 'center' }) => (
-  <Tag className="title" style={{ textAlign: align }}>
+const Title = ({
+  children, as: Tag = 'h1', className, align = 'center',
+}) => (
+  <Tag className={cn('title', className && className)} style={{ textAlign: align }}>
     {children}
   </Tag>
 )
