0c1333e0ff215a819570c29e90fd7b12149ea928
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-23 19:27:42 -0200

    feat: Add Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './loader.css'
+
+const Loader = () => (
+  <div className="loader">
+    <svg className="circular-loader" viewBox="25 25 50 50">
+      <circle className="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" strokeWidth="2" />
+    </svg>
+  </div>
+)
+
+export default Loader
