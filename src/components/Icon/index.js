7fcebe6e52f5d390b96125b3db0a8e4cd0fda672
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-24 02:05:11 -0200

    feat: Add Icons

---
+++ src/components/Icon/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './icon.css'
+
+const Icon = ({ name }) => (
+  <span className={`icon icon-${name}`} />
+)
+
+export default Icon
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -2,29 +2,37 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import cn from 'classnames'
 
+import Icon from '../Icon'
+
 import './side-bar.css'
 
 const SideBar = ({ onClick, shelf }) => (
   <aside className="side-bar">
-    <Link className="side-bar__button" to="/search">
-      Add a Book
-    </Link>
+    <div className="side-bar__action">
+      <Link className="side-bar__button" to="/search">
+        <Icon name="add" />
+        Add a Book
+      </Link>
+    </div>
     <span
       className={cn('side-bar__item', shelf === 'currentlyReading' && 'side-bar__item--selected')}
       onClick={() => onClick('currentlyReading')}
     >
+      <Icon name="book" />
       Currently Reading
     </span>
     <span
       className={cn('side-bar__item', shelf === 'wantToRead' && 'side-bar__item--selected')}
       onClick={() => onClick('wantToRead')}
     >
+      <Icon name="heart" />
       Want to Read
     </span>
     <span
       className={cn('side-bar__item', shelf === 'read' && 'side-bar__item--selected')}
       onClick={() => onClick('read')}
     >
+      <Icon name="archive" />
       Read
     </span>
   </aside>
