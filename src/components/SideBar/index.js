14c2703a1ce3c4e1e087f2790c939c133330d30a
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-24 16:38:20 -0200

    refactor: Update side bar styles

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -17,27 +17,29 @@ const SideBar = ({ onClick, shelf }) => (
         Add a Book
       </Link>
     </div>
-    <span
-      className={cn('side-bar__item', shelf === 'currentlyReading' && 'side-bar__item--selected')}
-      onClick={() => onClick('currentlyReading')}
-    >
-      <Icon name="book" />
-      Currently Reading
-    </span>
-    <span
-      className={cn('side-bar__item', shelf === 'wantToRead' && 'side-bar__item--selected')}
-      onClick={() => onClick('wantToRead')}
-    >
-      <Icon name="heart" />
-      Want to Read
-    </span>
-    <span
-      className={cn('side-bar__item', shelf === 'read' && 'side-bar__item--selected')}
-      onClick={() => onClick('read')}
-    >
-      <Icon name="archive" />
-      Read
-    </span>
+    <div>
+      <span
+        className={cn('side-bar__item', shelf === 'currentlyReading' && 'side-bar__item--selected')}
+        onClick={() => onClick('currentlyReading')}
+      >
+        <Icon name="book" />
+        Currently Reading
+      </span>
+      <span
+        className={cn('side-bar__item', shelf === 'wantToRead' && 'side-bar__item--selected')}
+        onClick={() => onClick('wantToRead')}
+      >
+        <Icon name="heart" />
+        Want to Read
+      </span>
+      <span
+        className={cn('side-bar__item', shelf === 'read' && 'side-bar__item--selected')}
+        onClick={() => onClick('read')}
+      >
+        <Icon name="archive" />
+        Read
+      </span>
+    </div>
   </aside>
 )
 
