ec1a540d85fa6ec2b3b2322324c18b335fc233b1
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-01-24 15:55:27 -0200

    refactor: Remove svg icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import cn from 'classnames'
 
 import './icon.css'
 
 const Icon = ({ name }) => (
-  <div className={`icon icon-${name}`} />
+  <i className={cn('icon', `icon-${name}`)} />
 )
 
 export default Icon
